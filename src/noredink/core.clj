ec2fc77fbd813e293b63bd256cf0afdffe4a7b01
Matt Valdez <mvaldez@gmail.com>
2014-12-12 15:40:39 -0800

    read questions from csv into seq

--- src/noredink/core.clj
+++ src/noredink/core.clj
@@ -1,8 +1,18 @@
-(ns noredink.core)
+(ns noredink.core
+  (require [clojure.data.csv :as csv]))
 
-(defn get-questions [num-questions]
+; TODO: reading all into memory because the data is small but could be done lazily
+(defn get-questions
+  "return seq of questions"
+  []
+  (with-open
+    [question-file (clojure.java.io/reader "questions.csv")]
+    (rest (doall (csv/read-csv question-file)))))
+
+(defn make-quiz
   "return seq of questions given number to return"
-  num-questions)
+  [num-questions]
+  "foo")
 
 ; TODO: change to loop until ctrl-c
 (defn -main
@@ -11,6 +21,5 @@
   (println "Enter number of questions to put in the quiz: ")
   (let [num-questions (read-string (read-line))]
     (if (and (number? num-questions) (> num-questions 0))
-      (println "Here you go: \n" (get-questions num-questions))
+      (println "Here you go: \n" (make-quiz num-questions))
       (println "Sorry, I need a positive integer."))))
-
