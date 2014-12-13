0289e791df0c71bce78da4ea660893820b318b00
Matt Valdez <mvaldez@gmail.com>
2014-12-13 13:20:46 -0800

    data structures in place

--- src/noredink/core.clj
+++ src/noredink/core.clj
@@ -1,42 +1,64 @@
 (ns noredink.core
   (require [clojure.data.csv :as csv]))
 
+; TODO: reading all into memory because the data is small but could be done lazily
+; TODO: move csv stuff to separate namespace
+; TODO: convert numeric strings to numbers
 (defn read-csv-file [filename cols]
+  "Return map with keys specified in cols. Assumes number of cols and fields match."
   (with-open [file (clojure.java.io/reader filename :encoding "ISO-8859-1")]
     (map #(zipmap cols %) (doall (csv/read-csv file)))))
 
-; TODO: reading all into memory because the data is small but could be done lazily
 (defn get-questions
   "return seq of questions"
   []
   (rest
-    (read-csv-file
-      "questions.csv"
-      [:strand_id :strand_name
-       :standard_id :standard_name
-       :question_id :difficulty])))
+    (read-csv-file "questions.csv"
+      [:strand_id
+       :strand_name
+       :standard_id
+       :standard_name
+       :question_id
+       :difficulty])))
 
 (defn get-usage
   "return seq of usage data"
   []
   (rest
-    (read-csv-file
-      "usage.csv"
-      [:student_id :question_id
-       :assigned_hours_ago :answered_hours_ago])))
+    (read-csv-file "usage.csv"
+      [:student_id
+       :question_id
+       :assigned_hours_ago
+       :answered_hours_ago])))
 
-(defn get-strand-ids
+
+; Presumably in production we'd have a similarly indexed tables
+(defn questions-by-strand-and-standard
   [questions]
-  (set (map :strand_id questions)))
+  (group-by #(vector (:strand_id %) (:standard_id %)) questions))
 
-(defn standard-ids-for-strand-id
+(defn get-strands
+  "Return vector of strands ids from the question data."
+  [questions]
+  (into [] (set (map :strand_id questions))))
+
+(defn get-standards-for-strand
+  "Return vector of standard ids for the given strand-id."
   [questions strand-id]
-  (set (map :standard_id (filter #(= strand-id (:strand_id %)) questions))))
+  (into [] (set (map :standard_id (filter #(= strand-id (:strand_id %)) questions)))))
+
+(defn standards-by-strand
+  "Return map of strands to standards."
+  [questions]
+  (let [strands (get-strands questions)
+        standards (map #(get-standards-for-strand questions %) strands)]
+    (zipmap strands standards)))
+
 
 (defn make-quiz
   "return seq of questions given number to return"
   [num-questions]
-  "foo")
+  num-questions)
 
 ; TODO: change to loop until ctrl-c
 (defn -main
