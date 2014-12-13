411c57caa197c641a83496fb22b084d71f9b1a8b
Matt Valdez <mvaldez@gmail.com>
2014-12-12 16:55:22 -0800

    tidy up csv reader

--- src/noredink/core.clj
+++ src/noredink/core.clj
@@ -1,14 +1,9 @@
 (ns noredink.core
   (require [clojure.data.csv :as csv]))
 
-(defn csv-fn [cols]
-  (fn [row]
-    (let [v (first (csv/read-csv row))]
-      (zipmap cols v))))
-
 (defn read-csv-file [filename cols]
   (with-open [file (clojure.java.io/reader filename :encoding "ISO-8859-1")]
-    (map (csv-fn cols) (doall (line-seq file)))))
+    (map #(zipmap cols %) (doall (csv/read-csv file)))))
 
 ; TODO: reading all into memory because the data is small but could be done lazily
 (defn get-questions
@@ -20,6 +15,23 @@
       [:strand_id :strand_name
        :standard_id :standard_name
        :question_id :difficulty])))
+
+(defn get-usage
+  "return seq of usage data"
+  []
+  (rest
+    (read-csv-file
+      "usage.csv"
+      [:student_id :question_id
+       :assigned_hours_ago :answered_hours_ago])))
+
+(defn get-strand-ids
+  [questions]
+  (map :strand_id questions))
+
+(defn standards-for-strand-id
+  [questions strand-id]
+  (map :strand_id (filter #(= strand-id (:strand_id %)))))
 
 (defn make-quiz
   "return seq of questions given number to return"
